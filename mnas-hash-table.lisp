ac9e06693b2877aad44d312311aa9d79bd67281f
Nick Matvyeyev <mnasoft@gmail.com>
2020-09-02 07:27:13 +0300

    Ср 02 сен 2020 04:27:13 UTC

--- mnas-hash-table.lisp
+++ mnas-hash-table.lisp
@@ -3,16 +3,13 @@
 (in-package #:cl-user)
 
 (defpackage #:mnas-hash-table
-  (:use #:cl ))
-
-;;;;(declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
+  (:use #:cl ) ;;;; #:cl-annot
+  (:export print-items hash-table-copy add-items to-list))
 
 (in-package #:mnas-hash-table)
 
-(annot:enable-annot-syntax)
-
-@export
-@annot.doc:doc
+(export 'add-items )
+(defun add-items (hash-table table-items key-index value-index)
 "@b(Описание:) add-items добавляет в hash-table элементы, находящиеся в таблице table-items
 в качестве ключей используются элементы, находящиеся в позиции key-index
 в качестве значений испоользуются элементы, находящиеся в позиции value-index.
@@ -32,8 +29,6 @@
  \"key_4\" \"Vval_4\"
 @end(code)
 "
-(defun add-items (hash-table table-items key-index value-index)
-  
   (mapc #'(lambda (el)
 	    (setf
 	     (gethash (nth key-index el) hash-table)
@@ -41,8 +36,13 @@
 	table-items)
   hash-table)
 
-@export
-@annot.doc:doc
+(export 'print-items )
+(defun print-items (hash-table
+		    &key
+		      (out t)
+		      (field-format "~S")
+		      (field-separator " ")
+		      (line-separator "~%") )
 "@b(Описание:) print-items выводит содержимое хеш-таблицы hash-table
 в поток out.
 
@@ -57,13 +57,6 @@
 	    0 2))
 @end(code)
 "
-(defun print-items (hash-table
-		    &key
-		      (out t)
-		      (field-format "~S")
-		      (field-separator " ")
-		      (line-separator "~%") )
-
   (maphash
    #'(lambda (k v)
        (format out
@@ -75,8 +68,8 @@
 	       k v))
    hash-table))
 
-@export
-@annot.doc:doc
+(export 'to-list )
+(defun to-list (hash-table)
 "@b(Описание:) to-list преобразует hash-table в список.
 
  @b(Пример использования:)
@@ -86,8 +79,6 @@
 	      '((1 2 3)(4 5 6)(7 8 9)) 1 2))
 @end(code)
 "
-(defun to-list (hash-table)
-
   (let ((rez nil))
     (maphash
      #'(lambda (k v)
@@ -95,11 +86,9 @@
      hash-table)
     rez))
 
-@export
-@annot.doc:doc
-"@b(Описание:) Создает копию хеш-таблицы."
+(export 'hash-table-copy )
 (defun hash-table-copy (table)
-  
+"@b(Описание:) Создает копию хеш-таблицы."
   (let ((new-table (make-hash-table
                     :test (hash-table-test table)
                     :size (hash-table-size table))))
